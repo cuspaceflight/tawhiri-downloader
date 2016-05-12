61390052405095d86294cb4c51cf6edc8b61f684
Daniel Richman <main@danielrichman.co.uk>
2016-05-12 00:21:15 +0000

    build instructions

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+# apt-get install libgrib-api-dev libffi-dev
+# opam pin cohttp https://github.com/danielrichman/ocaml-cohttp#v0.20.2-hotpatch
+# opam install core async cohttp ctypes ctypes-foreign 
+
 corebuild \
     -pkg ctypes.foreign \
     -pkg async \
